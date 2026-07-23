75a1b822f45855956e2142907b1b0c33340e6db0
khaiwic <khaideptrai13907@gmail.com>
2026-07-23 23:31:04 +0700

    hoan thanh xong lib header.h

--- include/header.h
+++ include/header.h
@@ -31,7 +31,47 @@
 #include <esp_task_wdt.h>
 
 //thu vien tu tao
+#include "PinConfig.h"
+#include "../lib/implement/led/led.h"
+#include "../lib/implement/wind/wind.h"
+#include "../lib/sensor/co2_metter/co2_metter.h"
+#include "../lib/sensor/Lux/lux_metter.h"
+#include "../lib/sensor/pms/pms7003.h"
+#include "../lib/sensor/sht30/sht30.h"
+#include "../lib/problem/reset/check_reset.h"   
+#include "../lib/problem/watch_dog/watchdog.h"
+#include "../lib/memory/sd.h"
+#include "../lib/server/messenge/messenge.h"
+#include "../lib/server/mqtt/mqtt.h"
+#include "../lib/server/wifi/wifi.h"
 
+//Wifi
+extern String ssid;
+extern String password;
 
+//MQTT
+extern String id;
+extern String mqtt_broker;
+extern String mqtt_username;
+extern String mqtt_password;
+extern int mqtt_port;
+extern String topic_pub;
+extern String topic_sub;
+
+//sensor and implement
+extern String uuid_gateway;
+extern String uuid_lux;
+extern String uuid_co2;
+extern String uuid_pm1;
+extern String uuid_pm25;
+extern String uuid_pm10;
+extern String uuid_temp;
+extern String uuid_humid;
+extern String uuid_wind_speed;
+extern String uuid_wind_direction;
+extern String uuid_rain;
+
+//Create key
+extern SemaphoreHandle_t xMutex_I2C;
 
 #endif
