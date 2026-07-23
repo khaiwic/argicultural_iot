2129dd454b1cecc7325139fb3b336e4ccb4397c6
khaiwic <khaideptrai13907@gmail.com>
2026-07-23 23:11:06 +0700

    them header va cac thu vien

---
+++ include/header.h
@@ -0,0 +1,37 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <BH1750.h>
+
+#include "esp_system.h"
+#include "MHZ19.h"
+
+//#include <SensirionI2cSHT3x.h> cam bien do am nhiet do
+
+#include <Wire.h>
+#include <Preferences.h>
+#include <PubSubClient.h> // giao thuc mqtt
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "freertos/semphr.h"
+#include "freertos/stream_buffer.h"
+#include "freertos/queue.h"
+#include "PMS.h"
+#include <string.h>
+#include "HardwareSerial.h"
+#include <SD.h>
+#include <SPI.h>
+#include <ArduinoJson.h>
+#include <esp_timer.h>
+#include <esp_task_wdt.h>
+
+//thu vien tu tao
+
+
+
+#endif
